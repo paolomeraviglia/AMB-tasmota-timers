dac9053aa48d836faf1fcb98445745eeb5f1b61a
paolomeraviglia <aixammimation@gmail.com>
2024-12-25 14:35:41 +0100

    Update interpreter.py
    changing non-berry behaviour

--- interpreter.py
+++ interpreter.py
@@ -77,8 +77,8 @@ while i < 17:
 
 berry += f'\n'+f'if status == 0 tasmota.cmd("Power1 0") else tasmota.cmd("Power1 1") end'
 fileCommand.write(output)
-output += f'\n'+f'Power1 '+str(status)
-fileNonBerry.write(output)
+stringNonBerry = f'Power1 '+str(status)
+fileNonBerry.write(stringNonBerry)
 fileBerry.write(berry)
 fileNonBerry.close()
 fileBerry.close()
